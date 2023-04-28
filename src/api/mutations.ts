e74762939b22962ef24c119648ee00ea1a5017b5
marchettomarcelo <marcelovfm@al.insper.edu.br>
2023-04-28 09:04:57 -0300

    form men

--- src/api/mutations.ts
+++ src/api/mutations.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-async function postPedidos({ nome, valor }: { nome: string; valor: number }) {
+async function postPedidos({ name, age }: { name: string; age: string }) {
   // return axios.get("http://localhost:3000/pedidos");
   console.log("bom dia");
-  return [{ id: 123, nome: nome, valor: valor }];
+  return [{ id: 123, nome: name, age: age }];
 }
 
 export { postPedidos };
