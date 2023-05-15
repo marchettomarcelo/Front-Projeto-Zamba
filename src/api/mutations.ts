ff74ed3021a12378aa21a4ea0cda8a1b16daed4a
marchettomarcelo <marcelovfm@al.insper.edu.br>
2023-05-15 20:33:46 -0300

    bom

--- src/api/mutations.ts
+++ src/api/mutations.ts
@@ -11,8 +11,8 @@ interface FormData {
 }
 
 
-const url = "http://192.168.10.117:8080/api/viagem"
-// const url = "http://localhost:8080/api/viagem"
+// const url = "http://192.168.10.117:8080/api/viagem"
+const url = "http://localhost:8080/api/viagem"
 
 async function postViagem(values: FormData) {
   try {
